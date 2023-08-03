7fb562c675bbb22ad4dc0905ab2d781cf0ac1711
ben-ka <ben10katz@gmail.com>
2023-08-03 11:22:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,37 +12,10 @@ import sys
 import datetime
 from flask import Flask,abort,jsonify, request
 from bs4 import BeautifulSoup
-
+from functions import GetLikes,ConvertToInt
 
 
 app = Flask(__name__)
-
-
-
-
-
-def GetLikes(driver,url):
-    driver.get(url)
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div #bottom-row")))      
-    elements = driver.find_elements(By.CSS_SELECTOR, "div .yt-core-attributed-string--white-space-no-wrap")
-    elements = [element.text for element in elements]
-    if 'Join' in elements:
-        video_likes = str(elements[5])
-    else:
-        video_likes = str(elements[4])
-    video_likes = ConvertToInt(video_likes)
-
-    return video_likes
-
-def ConvertToInt(string):
-    if "M" in string:
-        string = int(float(string[0 : string.index("M")]) * 1000000)
-    elif "K" in string:
-        string = int(float(string[0 : string.index("K")]) * 1000)
-    else:
-        string = int(string)
-
-    return string
 
 
 @app.route('/videos/<channel>', methods = ['GET'])
